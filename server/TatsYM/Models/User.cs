898036125b112a34777951ac9110415dd1aa806d
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-02-12 17:13:43 +0200

    AddIdentity (#8)
    * AddIdentity
    
    * NormalIdentity
    
    * Update🎉🎉🎉
    
    * Delete Role from UserEntity and commits

--- server/TatsYM/Models/User.cs
+++ server/TatsYM/Models/User.cs
@@ -1,13 +1,20 @@
 ﻿namespace TatsYum.Models.Users
 {
+
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Student = "Student";
+
+    }
     public class UserRegisterModel
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;  // Пароль для хешування
+        public string Password { get; set; } = string.Empty;
         public string? Avatar { get; set; }
-        public UserRole Role { get; set; } = UserRole.Student; // За замовчуванням студент
+        public string Role { get; set; } = UserRoles.Student;
         public DateTime DateOfBirth { get; set; }
     }
 }
