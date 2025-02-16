b66dd6f056e619997b1a008cd69c97a0d5e5f354
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-02-16 17:32:22 +0200

    fix role entity

---
+++ server/TatsYM/Migrations/20250212135355_New.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TatsYM.Migrations
+{
+    /// <inheritdoc />
+    public partial class New : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserEntityId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserEntityId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UserEntityId",
+                table: "AspNetUserRoles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserEntityId",
+                table: "AspNetUserRoles",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserEntityId",
+                table: "AspNetUserRoles",
+                column: "UserEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserEntityId",
+                table: "AspNetUserRoles",
+                column: "UserEntityId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
